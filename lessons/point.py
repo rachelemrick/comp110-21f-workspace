24eab70c5e3ea51c366634ec793573c09c34ab2c
Rachel Emrick <remrick@email.unc.edu>
2021-11-09 13:23:38 -0500

    LS25

--- lessons/point.py
+++ lessons/point.py
@@ -18,15 +18,45 @@ class Point:
 
     def scale(self, factor: float) -> Point:
         """Pure method that does not mutate the Point."""
-        x: float = self.x * factor
-        y: float = self.y * factor
-        scaled_point: Point = Point(x, y)
-        return scaled_point
+        return Point(self.x * factor, self.y * factor)
+    
+    # Magic Methods
+    def __str__(self) -> str:
+        """Produce a str representation of a Point for humans."""
+        return f"({self.x}, {self.y})"
+
+    def __repr__(self) -> str:
+        """Produce a str representation of a Point for Python!"""
+        return f"Point({self.x}, {self.y})"
+
+    # Operator Overload
+    def __mul__(self, factor: float) -> Point:
+        """Overload the multiplication operator for a Point * float."""
+        return Point(self.x * factor, self.y * factor)
+
+    def __add__(self, rhs: Point) -> Point:
+        """Overload the addition operator for a Point + Point."""
+        return Point(self.x + rhs.x, self.y + rhs.y)
+
+    def __getitem__(self, index: int) -> float:
+        """Overload the subscription notation."""
+        if index == 0:
+            return self.x
+        elif index == 1:
+            return self.y
+        else:
+            raise IndexError
 
 
 p0: Point = Point(1.0, 2.0)
-p0.scale_by(2.0)
-print(f"{p0.x}, {p0.y}")
+p1: Point = p0 * 2.0
+print(p0)
+print(p1)
 
-p1: Point = p0.scale(2.0)
-print(f"{p1.x}, {p1.y}")
+# Magic Methods
+p1_repr: str = repr(p1)
+print(p1_repr)
+
+# Operator Overload
+print(p0 + p1)
+print(p0[1])
